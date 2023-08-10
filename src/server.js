0629a89b1831feab40572877d1bb39ce4f1201af
Jan Rembikowski <janekrembikowski@gmail.com>
2023-08-10 18:15:42 +0200

    add cors to allow communication

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const { Ableton } = require("ableton-js");
 
 const ableton = new Ableton();
